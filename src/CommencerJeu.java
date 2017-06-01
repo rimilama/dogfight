c7a101338e0546d1cf0dcbe5ed25cb548d31fee3
antoi <antoi@DESKTOP-L1BGBGK>
2017-06-01 13:22:07 +0200

    bite

--- src/CommencerJeu.java
+++ src/CommencerJeu.java
@@ -1,9 +1,16 @@
-
+import dogfight.controller.DogfightController;
+import dogfight.model.DogfightModel;
+import dogfight.view.DogfightView;
+import gameframe.GamePanel;
 
 public class CommencerJeu {
 
 	public static void main(String[] args) {
-
+		final DogfightModel dogfightModel = new DogfightModel();
+		final DogfightController dogfightController = new DogfightController(dogfightModel);
+		final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel, dogfightModel);
+		dogfightController.setViewSystem(dogfightView);
+		dogfightController.play();
 	}
 
 }
