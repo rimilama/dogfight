0602f8dccf894b4ee67fbc1df257cc9b86ee7f03
Pierre Froidevaux <pierre@piexl.fr>
2017-06-01 13:14:30 +0200

    final 2

--- src/dogfight/model/Cloud.java
+++ src/dogfight/model/Cloud.java
@@ -7,7 +7,7 @@ public class Cloud extends Mobile {
 	private static final int WIDTH = 300;
 	private static final int HEIGHT = 150;
 	private static final String IMAGE = "cloud";
-	private static Position position = new Position();
+	private static Position position;
 
 
 	// Différentes méthodes
