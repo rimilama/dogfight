ad6ddf14e6bd57e0199b79014503279cac9085e5
antoi <antoi@DESKTOP-L1BGBGK>
2017-06-01 11:40:41 +0200

    phase 3 biatch

---
+++ src/dogfight/controller/DogfightController.java
@@ -0,0 +1,33 @@
+package dogfight.controller;
+
+public class DogfightController{
+	private final int TIME_SLEEP = 30;
+	
+	public DogfightController(IDogfightModel dogfightModel){
+		
+	}
+	
+	public void orderPermorm(UserOrder userOrder){
+		
+	}
+	
+	public void play(){
+		
+	}
+	
+	public void setViewSystem(IViewSystem viewSystem){
+		
+	}
+	
+	private void lauchMissile(int player){
+		
+	}
+	
+	public int getTIME_SLEEP() {
+		return TIME_SLEEP;
+	}
+
+	private void gameLoop(){
+		
+	}
+}
