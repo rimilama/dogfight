8f7598f197c11473d2ebead2da940c822f698893
antoi <antoi@DESKTOP-L1BGBGK>
2017-06-01 11:42:23 +0200

    phase biaaaaaaaaaaaaatch

---
+++ src/dogfight/controller/Order.java
@@ -0,0 +1,10 @@
+package dogfight.controller;
+
+public enum Order {
+	UP,
+	RIGHT,
+	DOWN,
+	LEFT,
+	SHOOT,
+	NOP,
+}
---
+++ src/dogfight/controller/UserOrder.java
@@ -0,0 +1,19 @@
+package dogfight.controller;
+
+public class UserOrder implements IUserOrder{
+	private int player;
+	private Order order;
+	
+	public UserOrder(int player, Order order){
+		this.player = player;
+		this.order = order;
+	}
+	
+	public Order getOrder(){
+		return order;
+	}
+	
+	public int getPlayer(){
+		return player;
+	}
+}
